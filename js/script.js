4083fcada9f4fafa8be96406f8ffb9c7a0128dcd
KaNiKir <kandzyuba@gmail.com>
2019-02-28 00:23:50 +0300

    Выполнено задание 9

---
+++ js/script.js
@@ -0,0 +1,61 @@
+  var link = document.querySelector(".map-button");
+  
+  var popup = document.querySelector(".feedback");
+  var close = popup.querySelector(".modal-close");
+  
+  var form = popup.querySelector("feedback-form");
+  var login = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=email]");
+  var content = popup.querySelector("[name=content]");
+  
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
+  });
+      
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
+  
